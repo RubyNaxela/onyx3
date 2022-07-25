408032a855bdc27021c5e1fd8a7927ebd4171e93
RubyNaxela <rubynaxela@gmail.com>
2022-07-25 18:23:06 +0200

    Changed the arrow type ui property

--- src/main/java/com/rubynaxela/onyx/Onyx.java
+++ src/main/java/com/rubynaxela/onyx/Onyx.java
@@ -1,12 +1,13 @@
 package com.rubynaxela.onyx;
 
 import com.formdev.flatlaf.intellijthemes.FlatDarkPurpleIJTheme;
-import com.rubynaxela.onyx.gui.window.MainWindow;
 import com.rubynaxela.onyx.gui.MaterialIcons;
+import com.rubynaxela.onyx.gui.window.MainWindow;
 import com.rubynaxela.onyx.io.I18n;
 import jiconfont.icons.google_material_design_icons.GoogleMaterialDesignIcons;
 import jiconfont.swing.IconFontSwing;
 
+import javax.swing.*;
 import java.util.Locale;
 
 /**
@@ -24,6 +25,7 @@ public class Onyx {
         IconFontSwing.register(GoogleMaterialDesignIcons.getIconFont());
         IconFontSwing.register(MaterialIcons.getIconFont());
         FlatDarkPurpleIJTheme.setup();
+        UIManager.put("Component.arrowType", "chevron");
         new MainWindow().setVisible(true);
 
 //        final var branch = new Operation.Branch(Category.OTHER, Monetary.valueOf(50f));
