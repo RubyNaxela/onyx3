0fe7dd0e6094f24efc653a8896dd7eb3951245d2
RubyNaxela <rubynaxela@gmail.com>
2022-07-28 01:30:55 +0200

    Expanded the Card and IconButton classes

--- src/main/java/com/rubynaxela/onyx/gui/components/Card.java
+++ src/main/java/com/rubynaxela/onyx/gui/components/Card.java
@@ -2,6 +2,7 @@ package com.rubynaxela.onyx.gui.components;
 
 import com.rubynaxela.onyx.util.Colors;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,21 +10,62 @@ import java.awt.*;
 public class Card extends JPanel {
 
     private final int padding;
+    private Color color;
+
+    public Card(@NotNull LayoutManager manager, int padding) {
+        super(manager);
+        this.padding = padding;
+        this.color = UIManager.getColor("Desktop.background").brighter();
+        setBackground(Colors.TRANSPARENT);
+    }
+
+    public Card(@NotNull LayoutManager manager) {
+        this(manager, 16);
+    }
 
     public Card(int padding) {
-        setBackground(Colors.TRANSPARENT);
-        this.padding = padding;
+        this(new FlowLayout(), padding);
     }
 
     public Card() {
         this(16);
     }
 
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(@Nullable Color color) {
+        this.color = color;
+    }
+
+    /**
+     * @return {@link Colors#TRANSPARENT}
+     * @deprecated Use {@link #getColor} instead.
+     */
+    @Deprecated
+    @Override
+    public Color getBackground() {
+        return super.getBackground();
+    }
+
+    /**
+     * This method should be called internally.
+     *
+     * @param color the desired background {@code Color}
+     * @deprecated Use {@link #setColor} instead.
+     */
+    @Deprecated
+    @Override
+    public void setBackground(@Nullable Color color) {
+        super.setBackground(color);
+    }
+
     @Override
     protected void paintComponent(@NotNull Graphics g) {
         final var insets = super.getInsets();
         final var graphics = (Graphics2D) g.create();
-        graphics.setColor(UIManager.getColor("Desktop.background").brighter());
+        graphics.setColor(color);
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         graphics.fillRoundRect(insets.left, insets.top, getWidth() - insets.left - insets.right,
                                getHeight() - insets.top - insets.bottom, 16, 16);
--- src/main/java/com/rubynaxela/onyx/gui/components/IconButton.java
+++ src/main/java/com/rubynaxela/onyx/gui/components/IconButton.java
@@ -1,10 +1,10 @@
 package com.rubynaxela.onyx.gui.components;
 
-import com.rubynaxela.onyx.util.Colors;
 import com.rubynaxela.onyx.util.ComponentUtils;
 import jiconfont.IconCode;
 import jiconfont.swing.IconFontSwing;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.border.CompoundBorder;
@@ -14,8 +14,16 @@ import java.awt.*;
 
 public class IconButton extends JButton {
 
+    private IconCode icon;
+    private float iconSize;
+    private Color iconColor;
+
     public IconButton(@NotNull String text, @NotNull IconCode icon, @NotNull Insets padding, @NotNull Insets border) {
-        super(" " + text, IconFontSwing.buildIcon(icon, 24f, Colors.LOGO_LIGHT_GRAY));
+        super(" " + text);
+        this.icon = icon;
+        this.iconSize = 24f;
+        this.iconColor = getForeground();
+        setIcon(IconFontSwing.buildIcon(icon, iconSize, iconColor));
         setBorder(new CompoundBorder(new MatteBorder(border, UIManager.getColor("Button.focusedBorderColor")),
                                      new EmptyBorder(padding)));
         ComponentUtils.setFontSize(this, 14f);
@@ -23,5 +31,35 @@ public class IconButton extends JButton {
 
     public IconButton(@NotNull String text, @NotNull IconCode icon, @NotNull Insets border) {
         this(text, icon, ComponentUtils.ZERO_INSETS, border);
+    }
+
+    /**
+     * Sets the icon. If {@code icon} is {@code null}, the icon will be removed.
+     *
+     * @param icon the new icon
+     */
+    public void setIcon(@Nullable IconCode icon) {
+        this.icon = icon;
+        setIcon(icon != null ? IconFontSwing.buildIcon(icon, iconSize, iconColor) : null);
+    }
+
+    /**
+     * Sets the icon size.
+     *
+     * @param size the new icon size
+     */
+    public void setIconSize(float size) {
+        this.iconSize = size;
+        if (icon != null) setIcon(IconFontSwing.buildIcon(icon, size, iconColor));
+    }
+
+    /**
+     * Sets the icon color. If {@code null}, the color returned by {@link #getForeground} is used.
+     *
+     * @param iconColor the desired icon color
+     */
+    public void setIconColor(@Nullable Color iconColor) {
+        this.iconColor = iconColor != null ? iconColor : getForeground();
+        if (icon != null) setIcon(IconFontSwing.buildIcon(icon, iconSize, iconColor));
     }
 }
