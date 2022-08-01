3f39c162aa5a0ad0b07fceffab3877b6716f090f
RubyNaxela <rubynaxela@gmail.com>
2022-08-02 00:35:10 +0200

    Updates on javadoc

--- src/main/java/com/rubynaxela/onyx/gui/components/Icon.java
+++ src/main/java/com/rubynaxela/onyx/gui/components/Icon.java
@@ -10,14 +10,34 @@ import java.awt.*;
 
 public class Icon extends JLabel {
 
+    /**
+     * Constructs an {@code Icon} with an {@link ImageIcon} created from the specified {@link ImageIcon}, size and color.
+     *
+     * @param icon  the icon to be used
+     * @param size  the icon size
+     * @param color the icon color
+     */
     public Icon(@NotNull IconCode icon, float size, @NotNull Color color) {
         super(IconFontSwing.buildIcon(icon, size, color));
     }
 
+    /**
+     * Constructs an {@code Icon} with an {@link ImageIcon} created from the specified
+     * {@link ImageIcon} and size. The icon's color is {@link Colors#LOGO_LIGHT_GRAY}.
+     *
+     * @param icon the icon to be used
+     * @param size the icon size
+     */
     public Icon(@NotNull IconCode icon, float size) {
         this(icon, size, Colors.LOGO_LIGHT_GRAY);
     }
 
+    /**
+     * Constructs an {@code Icon} with an {@link ImageIcon} created from the specified
+     * {@link IconCode}. The icon's size is 24pt and the color is {@link Colors#LOGO_LIGHT_GRAY}.
+     *
+     * @param icon the icon to be used
+     */
     public Icon(@NotNull IconCode icon) {
         this(icon, 24f);
     }
--- src/main/java/com/rubynaxela/onyx/io/I18n.java
+++ src/main/java/com/rubynaxela/onyx/io/I18n.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 import java.util.TreeMap;
 
 /**
- * Internationalizaion utility.
+ * Internationalization utility.
  */
 public final class I18n {
 
