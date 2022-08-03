24e3b4b13485d8401a36d2d812ad04005dc63eae
RubyNaxela <rubynaxela@gmail.com>
2022-08-04 01:51:31 +0200

    Type of Form#color changed to JColor

--- src/main/java/com/rubynaxela/onyx/data/Form.java
+++ src/main/java/com/rubynaxela/onyx/data/Form.java
@@ -1,5 +1,6 @@
 package com.rubynaxela.onyx.data;
 
+import com.rubynaxela.jadeite.awt.JColor;
 import com.rubynaxela.onyx.gui.MaterialIcons;
 import com.rubynaxela.onyx.io.I18n;
 import jiconfont.IconCode;
@@ -7,26 +8,25 @@ import jiconfont.swing.IconFontSwing;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.awt.*;
 
 public enum Form {
 
-    ATM_DEPOSIT(MaterialIcons.ATM, new Color(145, 198, 238)),
-    ATM_WITHDRAWAL(MaterialIcons.ATM, new Color(145, 198, 238)),
-    BLIK_PAYMENT(MaterialIcons.SMARTPHONE, new Color(215, 85, 111)),
-    CARD_PAYMENT(MaterialIcons.CREDIT_CARD, new Color(255, 198, 0)),
-    CARD_REVENUE(MaterialIcons.CREDIT_CARD, new Color(255, 198, 0)),
-    CASH_PAYMENT(MaterialIcons.PAYMENTS, new Color(62, 140, 69)),
-    CASH_REVENUE(MaterialIcons.PAYMENTS, new Color(62, 140, 69)),
-    INCOMING_TRANSFER(MaterialIcons.ACCOUNT_BALANCE, new Color(255, 98, 0)),
-    OTHER(MaterialIcons.GRID_VIEW, new Color(190, 190, 190)),
-    OUTGOING_TRANSFER(MaterialIcons.ACCOUNT_BALANCE, new Color(255, 98, 0));
+    ATM_DEPOSIT(MaterialIcons.ATM, new JColor(145, 198, 238)),
+    ATM_WITHDRAWAL(MaterialIcons.ATM, new JColor(145, 198, 238)),
+    BLIK_PAYMENT(MaterialIcons.SMARTPHONE, new JColor(215, 85, 111)),
+    CARD_PAYMENT(MaterialIcons.CREDIT_CARD, new JColor(255, 198, 0)),
+    CARD_REVENUE(MaterialIcons.CREDIT_CARD, new JColor(255, 198, 0)),
+    CASH_PAYMENT(MaterialIcons.PAYMENTS, new JColor(62, 140, 69)),
+    CASH_REVENUE(MaterialIcons.PAYMENTS, new JColor(62, 140, 69)),
+    INCOMING_TRANSFER(MaterialIcons.ACCOUNT_BALANCE, new JColor(255, 98, 0)),
+    OTHER(MaterialIcons.GRID_VIEW, new JColor(190, 190, 190)),
+    OUTGOING_TRANSFER(MaterialIcons.ACCOUNT_BALANCE, new JColor(255, 98, 0));
 
     public final IconCode icon;
-    public final Color color;
+    public final JColor color;
     public final ComboBoxView view;
 
-    Form(@NotNull IconCode icon, @NotNull Color color) {
+    Form(@NotNull IconCode icon, @NotNull JColor color) {
         this.icon = icon;
         this.color = color;
         this.view = new ComboBoxView();
